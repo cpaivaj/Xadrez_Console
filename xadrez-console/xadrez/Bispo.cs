59a8038028f3b79d257f4061b4af5c51a84ffc69
Carlos Paiva <carlos.alberto.2012@hotmail.com>
2017-08-27 17:35:19 -0300

    Adicionando as demais pecas

---
+++ xadrez-console/xadrez/Bispo.cs
@@ -0,0 +1,77 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Bispo : Peca
+    {
+        public Bispo(Tabuleiro tab, Cor cor) : base(tab, cor) { }
+
+        public override string ToString()
+        {
+            return "B";
+        }
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = tab.Peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // diagonal superior direita
+            pos.DefinirValores(posicao.linha - 1, posicao.coluna - 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.linha - 1, pos.coluna - 1);
+            }
+
+            // diagonal superior esqueda
+            pos.DefinirValores(posicao.linha - 1, posicao.coluna + 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.linha - 1, pos.coluna + 1);
+            }
+
+            // diagonal inferior direita
+            pos.DefinirValores(posicao.linha + 1, posicao.coluna + 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.linha + 1, pos.coluna + 1);
+            }
+
+            // diagonal inferior esquerda
+            pos.DefinirValores(posicao.linha + 1, posicao.coluna - 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.linha + 1, pos.coluna - 1);
+            }
+
+            return mat;
+        }
+    }
+}
---
+++ xadrez-console/xadrez/Dama.cs
@@ -0,0 +1,129 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Dama : Peca
+    {
+        public Dama(Tabuleiro tab, Cor cor) : base(tab, cor) { }
+
+        public override string ToString()
+        {
+            return "D";
+        }
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = tab.Peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // acima
+            pos.DefinirValores(posicao.linha - 1, posicao.coluna);
+            while (tab.PosicaoValida(pos) && PodeMover(pos)) // enquanto tiver casa livre e sem inimigo, pode mover
+            {
+                mat[pos.linha, pos.coluna] = true;
+                // se tiver uma peca de cor diferente para o loop
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != this.cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha - 1;
+            }
+
+            // abaixo
+            pos.DefinirValores(posicao.linha + 1, posicao.coluna);
+            while (tab.PosicaoValida(pos) && PodeMover(pos)) // enquanto tiver casa livre e sem inimigo, pode mover
+            {
+                mat[pos.linha, pos.coluna] = true;
+                // se tiver uma peca de cor diferente para o loop
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != this.cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 1;
+            }
+
+            // direita
+            pos.DefinirValores(posicao.linha, posicao.coluna + 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos)) // enquanto tiver casa livre e sem inimigo, pode mover
+            {
+                mat[pos.linha, pos.coluna] = true;
+                // se tiver uma peca de cor diferente para o loop
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != this.cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna + 1;
+            }
+
+            // esquerda
+            pos.DefinirValores(posicao.linha, posicao.coluna - 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos)) // enquanto tiver casa livre e sem inimigo, pode mover
+            {
+                mat[pos.linha, pos.coluna] = true;
+                // se tiver uma peca de cor diferente para o loop
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != this.cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+            }
+
+            // diagonal superior direita
+            pos.DefinirValores(posicao.linha - 1, posicao.coluna - 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.linha - 1, pos.coluna - 1);
+            }
+
+            // diagonal superior esqueda
+            pos.DefinirValores(posicao.linha - 1, posicao.coluna + 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.linha - 1, pos.coluna + 1);
+            }
+
+            // diagonal inferior direita
+            pos.DefinirValores(posicao.linha + 1, posicao.coluna + 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.linha + 1, pos.coluna + 1);
+            }
+
+            // diagonal inferior esquerda
+            pos.DefinirValores(posicao.linha + 1, posicao.coluna - 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.Peca(pos) != null && tab.Peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.DefinirValores(pos.linha + 1, pos.coluna - 1);
+            }
+
+            return mat;
+        }
+    }
+}
